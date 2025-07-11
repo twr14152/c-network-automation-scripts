14d8b378d07c2d6bdad95258714ddad22a4e67e8
Todd Riemenschneider <twr14152@gmail.com>
2025-07-11 10:08:55 -0400

    Create ssh_cli_script.c
    adding script to run commands on cisco device

---
+++ cisco/ssh_cli_script.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libssh/libssh.h>
+
+
+int main(int argc, char * argv[]) {
+  
+  // If you dont want to save the username and password in the script
+  // Start the for loop with i = 4
+  // Enter username and password after hostname when starting script
+  
+  for (int i = 4; i < argc; i++) {
+
+    printf("\nCommand: %s\n", argv[i]);
+
+    // Initialize SSH variables
+    ssh_session session;
+    int rc;
+
+    // Create SSH session
+    session = ssh_new();
+    if (session == NULL) {
+      fprintf(stderr, "Error creating SSH session\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Set SSH options
+    ssh_options_set(session, SSH_OPTIONS_HOST, argv[1]);
+    ssh_options_set(session, SSH_OPTIONS_USER, argv[2]); // or argv[2]
+
+    // Connect to SSH server
+    rc = ssh_connect(session);
+    if (rc != SSH_OK) {
+      fprintf(stderr, "Error connecting to SSH server: %s\n", ssh_get_error(session));
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
+
+    // Authenticate with password
+    rc = ssh_userauth_password(session, NULL, argv[3]); // or argv[3]
+    if (rc != SSH_AUTH_SUCCESS) {
+      fprintf(stderr, "Error authenticating with password: %s\n", ssh_get_error(session));
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
+
+    // Open channel for executing command
+    ssh_channel channel;
+    channel = ssh_channel_new(session);
+    if (channel == NULL) {
+      fprintf(stderr, "Error creating SSH channel\n");
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
+
+    // Execute command
+    rc = ssh_channel_open_session(channel);
+    if (rc != SSH_OK) {
+      fprintf(stderr, "Error opening SSH channel: %s\n", ssh_get_error(session));
+      ssh_channel_free(channel);
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
+
+    //This is where you loop through commands
+    rc = ssh_channel_request_exec(channel, argv[i]);
+    if (rc != SSH_OK) {
+      fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
+      ssh_channel_close(channel);
+      ssh_channel_free(channel);
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
+
+    // Read command output
+    char buffer[2048];
+    int nbytes;
+    while ((nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0)) > 0) {
+      fwrite(buffer, 1, nbytes, stdout);
+    }
+
+    // Close channel
+    ssh_channel_send_eof(channel);
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+
+    // Disconnect and free SSH session
+    ssh_disconnect(session);
+    ssh_free(session);
+  } //End of loop
+  printf("\n\n");
+  return 0;
+}

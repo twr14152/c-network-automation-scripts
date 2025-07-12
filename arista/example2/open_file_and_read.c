54056aa91c2311dd320c5e78fdf025334c8aa03d
Todd Riemenschneider <twr14152@gmail.com>
2025-07-12 10:18:55 -0400

    Update open_file_and_read.c

--- arista/example2/open_file_and_read.c
+++ arista/example2/open_file_and_read.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_CMDS 8192
+#define MAX_LINE 1024
+
 
 int main(int argc, char *argv[])
 {
-	char commands[2500];
+	char commands[MAX_CMDS];
+	char line[MAX_LINE]; 
+
 	FILE *fp = fopen(argv[3], "r");
 	if (!fp) 
 	{
 		printf("Error openning file.....\n");
 		return 1;
 	}
-	while (fgets(commands, sizeof(commands), fp) != NULL)
+	while (fgets(line, sizeof(line), fp) != NULL)
 	{
-		printf("%s", commands);
+		line[strcspn(line, "\r\n")] = 0;
+		if (strlen(line) == 0) continue;
+		strcat(commands, line);
+		strcat(commands, " ; ");
 	}
+	printf("%s", commands);
 	fclose(fp);
 	return 0;
 }

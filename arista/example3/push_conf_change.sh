b7ba401db680e7176b67dae6a04bbeb656cca385
Todd Riemenschneider <twr14152@gmail.com>
2025-07-14 17:37:57 -0400

    Create push_conf_change.sh
    script for pushing out change

---
+++ arista/example3/push_conf_change.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "admin" | ./ssh_script clab-lab3-ceos1 "admin" "configs/ceos1.cfg" 
+echo "admin" | ./ssh_script clab-lab3-ceos2 "admin" "configs/ceos2.cfg" 
+echo "admin" | ./ssh_script clab-lab3-ceos3 "admin" "configs/ceos3.cfg" 

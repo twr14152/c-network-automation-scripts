df75e75e31833f10fb7e3be0bb483a2483d17ce7
Todd Riemenschneider <twr14152@gmail.com>
2025-07-28 21:49:14 -0400

    Create wr_mem.sh
    script to write memory

---
+++ arista/httpClient/wr_mem.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+./http_client_using_files clab-lab3-ceos1 admin admin cmds3.txt
+./http_client_using_files clab-lab3-ceos2 admin admin cmds3.txt
+./http_client_using_files clab-lab3-ceos3 admin admin cmds3.txt
